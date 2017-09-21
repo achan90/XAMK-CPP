877d99ae0c1370d113df459c1334ec65e2057efc
Achan90 <pousi.pekka@gmail.com>
2017-09-21 22:23:34 +0300

    update

--- Project3/Project3/Source.cpp
+++ Project3/Project3/Source.cpp
@@ -3,8 +3,39 @@
 /*
 */
 
+void leapYear(int year) {
+	if (year % 4 == 0) {
+		if (year % 100 == 0) {
+			if (year % 400 == 0) {
+				std::cout << year << " on karkausvuosi." << std::endl;
+				std::cin.ignore();
+				std::cin.get();
+			}
+			else{ 
+				std::cout << year << " ei ole karkausvuosi." << std::endl;
+				std::cin.ignore();
+				std::cin.get();
+			}
+		}
+	}
+	else {
+		std::cout << year << " ei ole karkausvuosi." << std::endl;
+		std::cin.ignore();
+		std::cin.get();
+	}
+}
 
+int main(void){
+	int year = 0;
 
-int main(void) {
+	std::cout << "Anna vuosiluku:" << std::endl;
+	while (!(std::cin >> year)) {
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "Anna vuosiluku:" << std::endl;
+	}
+
+	leapYear(year);
+
 	return 0;
 }
