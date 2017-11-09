5ed0584e2d33abaec4b3b3f9791d65b79df63f1a
Achan90 <pousi.pekka@gmail.com>
2017-11-09 08:31:13 +0200

    update

--- Project14/Project14/Source.cpp
+++ Project14/Project14/Source.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 #include <string>
+#include <regex>
 
 int main(void) {
-	std::string str;
+	std::string input;
+	std::string output;
+	std::regex rgx1("ä");
+	std::regex rgx2("Ä");
 	
-	std::cout << "Syota rivi tekstia: "
+	while (true) {
+		std::cout << std::endl << "Syota rivi tekstia: ";
+		std::getline(std::cin, input);
+		output = std::regex_replace(input, rgx1, "a");
+		output = std::regex_replace(input, rgx2, "A");
+		std::cout << std::endl << output << std::endl;
+
+		if (!(std::regex_match(input, rgx1))) {
+			if (!(std::regex_match(input, rgx2))) {
+				break;
+			}
+		}
+	}
+	system("pause");
+	return 0;
 }
