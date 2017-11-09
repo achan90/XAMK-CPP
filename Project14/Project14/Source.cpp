7917cf977912455ed75a517b5f6535392ed5be4b
Pekka Pousi <opepo002@edu.xamk.fi>
2017-11-09 14:23:56 +0200

    dkpojakdaps

--- Project14/Project14/Source.cpp
+++ Project14/Project14/Source.cpp
@@ -2,25 +2,39 @@
 #include <string>
 #include <regex>
 
-int main(void) {
-	std::string input;
-	std::string output;
-	std::regex rgx1("ä");
-	std::regex rgx2("Ä");
-	
-	while (true) {
-		std::cout << std::endl << "Syota rivi tekstia: ";
-		std::getline(std::cin, input);
-		output = std::regex_replace(input, rgx1, "a");
-		output = std::regex_replace(input, rgx2, "A");
-		std::cout << std::endl << output << std::endl;
+void start(std::string input) {
+	std::regex rgxArray[4] = { std::regex("\x84"),std::regex("\x8e"),std::regex("\x94"),std::regex("\x99") };
 
-		if (!(std::regex_match(input, rgx1))) {
-			if (!(std::regex_match(input, rgx2))) {
-				break;
-			}
+	input = std::regex_replace(input, rgxArray[0], "a");
+	input = std::regex_replace(input, rgxArray[1], "A");
+	input = std::regex_replace(input, rgxArray[2], "ö");
+	input = std::regex_replace(input, rgxArray[3], "Ö");
+
+	std::cout << std::endl << input << std::endl;
+
+	std::cout << "Syötä rivi tekstiä: " << std::endl;
+	std::getline(std::cin, input);
+
+	for (int x = 0; x < 4; x++) {
+		if (std::regex_match(input, rgxArray[x]) == true) {
+			start(input);
+		}
+		else {
+			return;
 		}
 	}
+
+}
+
+int main(void) {
+	std::string input;	
+
+	std::cout << "Syötä rivi tekstiä: " << std::endl;
+	std::getline(std::cin, input);
+
+	start(input);
+
+	std::cout << std::endl;
 	system("pause");
 	return 0;
 }
