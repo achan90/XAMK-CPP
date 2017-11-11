4aff48dca66b8134b1c26a9e848c383ca997c58c
Achan90 <pousi.pekka@gmail.com>
2017-11-11 18:48:10 +0200

    update

--- Project16/Project16/Source.cpp
+++ Project16/Project16/Source.cpp
@@ -6,20 +6,26 @@ int main(void) {
 	std::string str;
 	bool check = true;
 	
-	std::cout << std::endl << "Syota rivi tekstia: ";
-	std::getline(std::cin, str);
+	do {
+		std::cout << std::endl << "Syota rivi tekstia: ";
+		std::getline(std::cin, str);
 
-	for (int i = 0; i < str.length(); i++) {
-		if (check == true) {
-			check = false;
-			str.replace(i, 1, "x");
+		for (int i = 0; i < str.length(); i++) {
+			if (str[i] == ' ') {
+				check = false;
+			}
+			else if (check == true) {
+				check = false;
+				str.replace(i, 1, "x");
+			}
+			else if (check == false) {
+				check = true;
+			}
 		}
-		else if (check == false) {
-			check = true;
-		}
-	}
-	std::cout << str << std::endl << std::endl;
+		check = true;
+		std::cout << str << std::endl;
+	} while (!(str.empty()));	
+	
 	system("pause");
-
 	return 0;
 }
