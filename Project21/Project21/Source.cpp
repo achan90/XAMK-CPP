b406d15859de44432011046d2904198b437893b1
Achan90 <pousi.pekka@gmail.com>
2017-11-29 17:49:16 +0200

    update

---
+++ Project21/Project21/Source.cpp
@@ -0,0 +1,16 @@
+/*
+Tee ohjelma, joka sijoittaa alustusvaiheessa 10 kokonaislukua 10-alkioiseen taulukkoon. 
+Lisäksi ohjelma pyytää yhden kokonaisluvun ”etsittava” ja ohjelma selvittää, löytyykö ”etsittava” taulukosta. 
+Jos löytyy se poistetaan ja kaikkia ”etsittava”:n  jälkeen tulevia taulukon alkioita siirretään yksi elementti kohti 0-elementtiä. 
+Taulukon loppuun sijoitetaan arvo 0.  
+Lopuksi taulukko tulostetaan.
+*/
+
+#include <iostream>
+#include <string>
+
+int main(void) {
+	int array[] = { 1,2,3,4,5,6,7,8,9,10 };
+
+
+}
