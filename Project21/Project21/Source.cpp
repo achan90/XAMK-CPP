4833738cbfbc127390b1bc1f3331f81d80971768
Pekka Pousi <opepo002@edu.xamk.fi>
2017-11-30 12:59:25 +0200

    KULIDKAJOSIDJPOJAFP

--- Project21/Project21/Source.cpp
+++ Project21/Project21/Source.cpp
@@ -11,6 +11,32 @@ Lopuksi taulukko tulostetaan.
 
 int main(void) {
 	int array[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int num = 0;
+	int index = 0;
+	bool check = false;
 
+	std::cout << "Anna etsittava numero: ";
+	std::cin >> num;
 
+	for (int i = 0; i < 10; i++) {
+		if (array[i] == num) {
+			index = i;
+			check = true;
+		}
+	}
+	if (check == true) {
+		for (int i = index; i < 10; i++) {
+			array[i] = array[i + 1];
+		}
+	}
+	if (check == true) {
+		array[9] = 0;
+	}
+	for (int i = 0; i < 10; i++) {
+		std::cout << array[i] << " ";
+	}
+	std::cout << std::endl;
+
+	system("pause");
+	return EXIT_SUCCESS;
 }
